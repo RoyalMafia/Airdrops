e953e1289ef803810157e069027ebf660ab274a7
RoyalMafia <benpotten@gmail.com>
2016-10-30 22:27:27 +0000

    Update shared.lua

--- lua/entities/dropent/shared.lua
+++ lua/entities/dropent/shared.lua
@@ -3,7 +3,7 @@ ENT.Base = "base_gmodentity"
 ENT.PrintName= "Airdrop ENT"
 ENT.Category = "Airdrop" 
 ENT.Author= "Ryl"
-ENT.Spawnable = true
+ENT.Spawnable = false
 ENT.AdminSpawnable = true
 
 function ENT:SetupDataTables()
